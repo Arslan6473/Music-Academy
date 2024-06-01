6316921e327d8b353cfd00fa70ad48647cfa8ec7
Arslan Sarwar <arslansarwarwebdev@mail.com>
2024-06-01 03:30:12 -1200

    some css changes

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -4,7 +4,7 @@ import { WavyBackground } from "./ui/wavy-background"
 import courseData from '../data/musicCourses.json'
 function Instructors() {
   return (
-    <div className="relative h-[38rem] overflow-hidden flex items-center justify-center">
+    <div className="relative h-[36rem] overflow-hidden flex items-center justify-center">
         <WavyBackground>
         <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">Meet Our Instructors</h2>
             <p className="text-base md:text-lg text-white text-center mb-4">Discover the talented professionals who will guide your musical journey</p>
